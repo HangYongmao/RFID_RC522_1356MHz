0cc6e42cb625b362c5d2955edbc89fd46cde36c4
HangYongmao <1981217256@qq.com>
2019-04-01 20:35:59 +0800

    完成卡号的读取.

---
+++ Keil/Src/TIMER.c
@@ -0,0 +1,34 @@
+#include <reg52.h>
+#include <intrins.h>
+
+unsigned char clock=0;
+unsigned char memu;
+extern bit clsFlag;
+
+void InitTimer0()
+{
+    TMOD &= 0xf0;
+    TH0 = (8192-4607)/32;
+	TL0 = (8192-4607)%32;
+	EA = 1;
+	ET0 = 1;
+	TR0 = 1;
+}
+
+void T0_time() interrupt 1
+{
+    static unsigned char num;
+	TH0 = (8192-4607)/32;
+	TL0 = (8192-4607)%32;
+	num++;
+    if(num%200 == 0)
+    {
+        num = 0;
+        clock ++;
+    }
+    if ((clock == 5) && (memu != 0))
+    {
+        clock = 0;
+        clsFlag = 1;
+    }
+}
---
+++ Keil/Src/main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+//////////////////////////////////
+//¶Ë¿Ú¶¨Òå
+
+sbit MF522_SDA  = P2 ^ 7; //SDA
+sbit MF522_SCK  = P2 ^ 6; //SCK
+sbit MF522_MOSI = P2 ^ 5; //MOSI
+sbit MF522_MISO = P2 ^ 4; //MISO
+sbit MF522_RST  = P2 ^ 3; //RST
+
+#endif
